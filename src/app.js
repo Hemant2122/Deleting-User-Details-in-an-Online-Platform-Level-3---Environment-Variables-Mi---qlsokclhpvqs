c1df9027bb9388f8598aa5d2710c0982c74d4921
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-06-19 16:43:36 +0530

    some code changes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,133 @@
+// const fs = require("fs");
+// const express = require("express");
+// const app = express();
+
+// // Importing products from userDetails.json file
+// const userDetails = JSON.parse(
+//   fs.readFileSync(`${__dirname}/data/userDetails.json`)
+// );
+
+// //Middlewares
+// app.use(express.json());
+
+// //Write DELETE endpoint for deleting the details of user
+// app.delete("/api/v1/details/:id", (req, res) => {
+//   const id = Number(req.params["id"]);
+//   const product = userDetails.find((product) => product.id === id);
+//   console.log(product, 'products');
+//   if(!product){
+//     res.statusCode = 404;
+//     const output = { "status": "failed", "message": "User not found!" };
+//     res.send(output);
+//   }
+
+//   const index = userDetails.indexOf(product);
+//   userDetails.splice(index, 1);
+//   fs.writeFile(
+//     `${__dirname}/data/userDetails.json`,
+//     JSON.stringify(userDetails),
+//     (error) => {
+//       res.statusCode = 200;
+//       const output = {
+//         status: "success", 
+//         message: "User details deleted successfully",
+//         data: {
+//           details: product,
+//         }
+//       }
+//       res.send(output);
+//     }
+//   )
+// });
+
+// // PATCH endpoint for editing user details
+// app.patch("/api/v1/details/:id", (req, res) => {
+//   const id = req.params.id * 1;
+//   const updatedDetails = userDetails.find(
+//     (updatedDetails) => updatedDetails.id === id
+//   );
+//   const index = userDetails.indexOf(updatedDetails);
+//   if (!updatedDetails) {
+//     return res.status(404).send({
+//       status: "failed",
+//       message: "User not found!",
+//     });
+//   }
+
+//   Object.assign(updatedDetails, req.body);
+
+//   fs.writeFile(
+//     `${__dirname}/data/userDetails.json`,
+//     JSON.stringify(userDetails),
+//     (err) => {
+//       res.status(200).json({
+//         status: "success",
+//         message: `User details updated successfully for id: ${updatedDetails.id}`,
+//         data: {
+//           updatedDetails,
+//         },
+//       });
+//     }
+//   );
+// });
+
+// // POST endpoint for registering new user
+// app.post("/api/v1/details", (req, res) => {
+//   const newId = userDetails[userDetails.length - 1].id + 1;
+//   const { name, mail, number } = req.body;
+//   const newUser = { id: newId, name, mail, number };
+//   userDetails.push(newUser);
+//   fs.writeFile(
+//     `${__dirname}/data/userDetails.json`,
+//     JSON.stringify(userDetails),
+//     (err) => {
+//       res.status(201).json({
+//         status: "Success",
+//         message: "User registered successfully",
+//         data: {
+//           userDetails: newUser,
+//         },
+//       });
+//     }
+//   );
+// });
+
+// // GET endpoint for sending the details of users
+// app.get("/api/v1/details", (req, res) => {
+//   res.status(200).json({
+//     status: "Success",
+//     message: "Detail of users fetched successfully",
+//     data: {
+//       userDetails,
+//     },
+//   });
+// });
+
+// // GET endpoint for sending the details of users by id
+// app.get("/api/v1/userdetails/:id", (req, res) => {
+//   let { id } = req.params;
+//   id *= 1;
+//   const details = userDetails.find((details) => details.id === id);
+//   if (!details) {
+//     return res.status(404).send({
+//       status: "failed",
+//       message: "User not found!",
+//     });
+//   } else {
+//     res.status(200).send({
+//       status: "success",
+//       message: "Details of users fetched successfully",
+//       data: {
+//         details,
+//       },
+//     });
+//   }
+// });
+
+// module.exports = app;
+
+
+
 const fs = require("fs");
 const express = require("express");
 const app = express();
@@ -65,26 +195,23 @@ app.patch("/api/v1/details/:id", (req, res) => {
 
 // POST endpoint for registering a new user
 app.post("/api/v1/details", (req, res) => {
+  const newId = userDetails[userDetails.length - 1].id + 1;
   const { name, mail, number } = req.body;
-  if (!name || !mail || !number) {
-    return res.status(400).json({ status: "failed", message: "Missing required fields!" });
-  }
-
-  const newId = (userDetails[userDetails.length - 1]?.id || 0) + 1;
   const newUser = { id: newId, name, mail, number };
   userDetails.push(newUser);
-
-  fs.writeFile(userDetailsFilePath, JSON.stringify(userDetails, null, 2), (err) => {
-    if (err) {
-      console.error("Failed to write to userDetails.json:", err);
-      return res.status(500).json({ status: "error", message: "Failed to register user!" });
+  fs.writeFile(
+    `${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) => {
+      res.status(201).json({
+        status: "Success",
+        message: "User registered successfully",
+        data: {
+          userDetails: newUser,
+        },
+      });
     }
-    res.status(201).json({
-      status: "success",
-      message: "User registered successfully",
-      data: { userDetails: newUser },
-    });
-  });
+  );
 });
 
 // GET endpoint for sending the details of users
@@ -119,3 +246,4 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
